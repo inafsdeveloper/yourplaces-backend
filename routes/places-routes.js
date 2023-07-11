5baa65048c7004321e85ac786513a94156f7e79d
inafsdeveloper <inafsdeveloper@gmail.com>
2023-07-11 16:44:50 -0700

    Adding Place-Specific Routes

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -2,9 +2,15 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  console.log('GET Request in Places');
-  res.json({message: 'It works!'});
+
+const DUMMY_PLACES = require('../shared/data/places.json');
+
+router.get('/:pid', (req, res, next) => {
+    console.log('GET Request in Places');
+    //   console.log(req);
+    const placeId = req.params.pid;
+    const place = DUMMY_PLACES.find(p => p.id === placeId);
+    res.json({ place });
 });
 
 module.exports = router;
