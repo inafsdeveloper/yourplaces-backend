9e2d609d8e081c9a70a2987cb6ab3e5f3bf18501
inafsdeveloper <inafsdeveloper@gmail.com>
2023-07-11 23:56:26 -0700

    Handling errors for unsupported routes

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const HttpError = require('./models/http-error');
 
 const placesRoutes = require('./routes/places-routes');
 
 const app = express();
 
 app.use(bodyParser.json());
+
 app.use('/api/places', placesRoutes); // => /api/places/...
+
+app.use((req, res, next) => {
+    throw new HttpError('Could not found route.', 404); 
+});
 
 app.use((error, req, res, next) => {
     if (req.headerSent) {
