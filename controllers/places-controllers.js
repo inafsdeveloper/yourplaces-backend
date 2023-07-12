4cb9da846b0bbb456ff6c0a1a634351c574e1978
inafsdeveloper <inafsdeveloper@gmail.com>
2023-07-12 00:10:18 -0700

    Adding Patch Routes to Update Place

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -36,9 +36,9 @@ const getPlaceByUserId = (req, res, next) => {
 };
 
 const createPlace = (req, res, next) => {
-  const {title, imageUrl, description, address, coordinates, creator } = req.body;
+  const { title, imageUrl, description, address, coordinates, creator } = req.body;
   const createdPlace = {
-    u1: uuidv4(),
+    id: uuidv4(),
     title,
     imageUrl,
     description,
@@ -54,6 +54,28 @@ const createPlace = (req, res, next) => {
   });
 }
 
+const updatePlace = (req, res, next) => {
+  const { title, description } = req.body;
+
+  const placeId = req.params.pid;
+  const updatedPlace = { ...DUMMY_PLACES.find(p => p.id === placeId) };
+  const placeIndex = DUMMY_PLACES.findIndex(p => p.id === placeId);
+
+  updatedPlace.title = title;
+  updatedPlace.description = description;
+
+  DUMMY_PLACES[placeIndex] = updatedPlace;
+
+  res.status(200).json({place: updatedPlace});
+  
+};
+
+const deletePlace = (req, res, next) => {
+
+}
+
 exports.getPlaceById = getPlaceById;
 exports.getPlaceByUserId = getPlaceByUserId;
 exports.createPlace = createPlace;
+exports.updatePlace = updatePlace;
+exports.deletePlace = deletePlace;
