37613a976b44b01e2d6bfb2ef2e5842ae2cd17be
inafsdeveloper <inafsdeveloper@gmail.com>
2023-07-11 23:51:50 -0700

    Adding a POST Route

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -1,5 +1,5 @@
 const HttpError = require('../models/http-error');
-
+const { v4: uuidv4 } = require('uuid');
 const DUMMY_PLACES = require('../shared/data/places.json');
 
 const getPlaceById = (req, res, next) => {
@@ -35,5 +35,25 @@ const getPlaceByUserId = (req, res, next) => {
   res.json({ places });
 };
 
+const createPlace = (req, res, next) => {
+  const {title, imageUrl, description, address, coordinates, creator } = req.body;
+  const createdPlace = {
+    u1: uuidv4(),
+    title,
+    imageUrl,
+    description,
+    address,
+    creator,
+    location: coordinates
+  };
+
+  DUMMY_PLACES.push(createdPlace);
+
+  res.status(201).json({
+    place: createdPlace
+  });
+}
+
 exports.getPlaceById = getPlaceById;
 exports.getPlaceByUserId = getPlaceByUserId;
+exports.createPlace = createPlace;
