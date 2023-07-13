02a4c01a67189aec4569af435fdbe024700959fe
inafsdeveloper <inafsdeveloper@gmail.com>
2023-07-13 00:39:43 -0700

    Getting Places - alternative

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -32,22 +32,22 @@ const getPlaceById = async (req, res, next) => {
 const getPlaceByUserId = async (req, res, next) => {
   const userId = req.params.uid;
 
-  let places;
+  let userWithPlaces;
 
   try {
-    places = await Place.find({ creator: userId });
+    userWithPlaces = await User.findById(userId).populate('places');
   } catch (err) {
     const error = new HttpError(`Fetching places failed. Please try again. Details:[${err}]`, 500);
     return next(error);
   }
 
-  if (!places || places.length === 0) {
+  if (!userWithPlaces || userWithPlaces.length === 0) {
     return next(
       new HttpError('Could not find a place for the provided user id.', 404)
     );
   }
 
-  res.json({ places: places.map(place => place.toObject({ getters: true })) });
+  res.json({ places: userWithPlaces.places.map(place => place.toObject({ getters: true })) });
 };
 
 const createPlace = async (req, res, next) => {
