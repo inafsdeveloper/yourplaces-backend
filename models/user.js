e6fd06aa4351522dff58be4a9dd5d5fa65eeb779
inafsdeveloper <inafsdeveloper@gmail.com>
2023-07-13 11:56:22 -0700

    Fixed Password in User Model

--- models/user.js
+++ models/user.js
@@ -6,7 +6,7 @@ const userSchema = new Schema({
         name: {type: String, required: true},
         email: {type: String, required: true, unique: true},
         image: {type: String, required: true},
-        password: {type: String, required: true, minlength: 6, select: false},
+        password: {type: String, required: true, minlength: 6},
         places: [{type: mongoose.Types.ObjectId, required: true, ref: 'Place'}]
 });
 
