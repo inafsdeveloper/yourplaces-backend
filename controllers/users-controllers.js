cbb40e31ee9078ac1ff7fba737393010cb06cc92
inafsdeveloper <inafsdeveloper@gmail.com>
2023-07-12 18:07:14 -0700

    Login User Added

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -51,23 +51,30 @@ const signup = async (req, res, next) => {
     res.status(201).json({ user: createdUser.toObject({getters: true})});
 };
 
-const login = (req, res, next) => {
+const login = async (req, res, next) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
         console.log(errors);
-        throw new HttpError('Invalid inputs passed, please check your data.', 422);
+        return next(new HttpError('Invalid inputs passed, please check your data.', 422));
     }
 
     const { email, password } = req.body;
 
-    const user = DUMMY_USERS.find(user => user.email === email);
+    let existingUser;
 
-    if (!user || password !== user.password) {
-        throw new HttpError('Could not identify user, credentials seem to be wrong.', 401);
+    try {
+        existingUser = await User.findOne({email: email});
+    } catch(err) {
+        const error = new HttpError(`Logging in failed, please try again later.\n Details \n [${err}]`, 500);
+        return next(error);
     }
 
+    
 
+    if (!existingUser || password !== existingUser.password) {
+        return next(new HttpError('Could not identify user, credentials seem to be wrong.', 401));
+    }
 
     res.json({ message: "Logged in!" });
 
