88fa2116713b25dcc7509dfa868abfc4bacf153d
inafsdeveloper <inafsdeveloper@gmail.com>
2023-07-14 13:47:50 -0700

    Use Multer to Save files and Apply filtering files on the backend (images only)

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const { check } = require('express-validator')
 const usersControllers = require('../controllers/users-controllers');
+const fileUpload = require('../middleware/file-upload');
 
 const router = express.Router();
 
 router.get('/', usersControllers.getUsers);
 
 router.post('/signup',
+    fileUpload.single('image'),
     [
         check('name').not().isEmpty().withMessage("Name is required."),
         check('email').isEmail().withMessage("Email Address is not valid!"),
         check('password').not().isEmpty().withMessage("Password is required.")
-        .isLength({min: 6}).withMessage("Password should be atleast 6 characters")
+            .isLength({ min: 6 }).withMessage("Password should be atleast 6 characters")
     ],
     usersControllers.signup);
 
